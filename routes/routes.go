1608dca3090454cfcae99e28a6fe4068e84bde4d
mooxun <mooxun@126.com>
2020-11-26 11:19:13 +0800

    add 增加静态资源路由

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	homeV1 "github.com/mooxun/emgo-web/routes/api/home/v1"
 	platformV1 "github.com/mooxun/emgo-web/routes/api/platform/v1"
@@ -19,6 +21,19 @@ func loader(items ...Routers) {
 // 路由注册
 func register() *gin.Engine {
 	r := gin.Default()
+	r.StaticFile("/", "emgo-admin/index.html")
+	r.StaticFile("/color.less", "emgo-admin/color.less")
+	r.StaticFile("/favicon.ico", "emgo-admin/favicon.ico")
+	r.Static("/css", "emgo-admin/css")
+	r.Static("/js", "emgo-admin/js")
+	r.Static("/img", "emgo-admin/img")
+	r.Static("/loading", "emgo-admin/loading")
+
+	r.LoadHTMLFiles("emgo-admin/index.html")
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
 	r.Use(middleware.ErrorHandler(), middleware.Cors())
 
 	for _, item := range routers {
